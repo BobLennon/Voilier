27ee1c07df389e598ccf7258aae0d172e73dcce2
root <couronne@lacroixrouge-brest.fr>
2017-10-10 11:55:12 +0200

    aplémentation de latitude

--- serveur.py
+++ serveur.py
@@ -2,7 +2,7 @@ import socket
 PORT=5005
 #IPSERV="192.168.0.228" #IP Raspi
 IPSERV="127.0.0.1"
-lat=5749743
+lat=5.749743
 a=0
 b=0
 c=0
@@ -16,10 +16,11 @@ while True:
         print "leng : ",ord(data[1])
         print "gv : ",ord(data[2])
         print "safran : ",ord(data[3])
-        a=(bit32tobit24)delat
-        b=(bit23tobit16)delat
-        c=(bit15tobit8)delat
-        d=(bit7tobit0)delat
+        lat=(int)(lat*1000000)
+        a=(lat>>24)&0XFF
+        b=(lat>>16)&0xFF
+        c=(lat>>8)&0XFF
+        d=lat&0xFF
         
         sock.sendto(bytearray([88,4,10,180,001,a,b,c,d]),addr)
 

434ea4b21551604088c19daa9f1bc0e652ae3ca4
root <couronne@lacroixrouge-brest.fr>
2017-10-11 15:59:36 +0200

    ajout de longitude et essai sur -

--- client.py
+++ client.py
@@ -3,24 +3,32 @@ import socket
 IPDEST="127.0.0.1"
 PORT=5005
 lat=0
+longi=0
+longi1=0b11111111111111111111111111111111
+longi2=0b00000000000000000000000000000001
 sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.sendto(bytearray([88,2,30,90]),(IPDEST,PORT))
 
 
 while True:
-        data,addr=sock.recvfrom(10)
+        data,addr=sock.recvfrom(15)
         print "id : ",ord(data[0])
         print "longeur : ",ord(data[1])
         print "v.vent : ",ord(data[2])
         print "d.vent : ",ord(data[3])
         print "test : ",ord(data[4])
-        print ord(data[5])
-        print ord(data[6])
-        print ord(data[7])
-        print ord(data[8])
         lat=(float)(ord(data[5])<<24)
         lat=lat+(ord(data[6])<<16)
         lat=lat+(ord(data[7])<<8)
         lat=lat+ord(data[8])
         lat=lat/1000000
         print "Latitude = ",lat
+        
+        longi=(float)(ord(data[9])<<24)
+        longi=longi+(ord(data[10])<<16)
+        longi=longi+(ord(data[11])<<8)
+        longi=longi+ord(data[12])
+        longi=longi/1000000
+        longi=longi+longi1
+        longi=longi+longi2
+        print "Longitude = ",longi
--- serveur.py
+++ serveur.py
@@ -2,16 +2,22 @@ import socket
 PORT=5005
 #IPSERV="192.168.0.228" #IP Raspi
 IPSERV="127.0.0.1"
-lat=5.749743
+lat=48.409484
+longi=-4.487820
+longi1=0b11111111111111111111111111111111
+longi2=0b00000000000000000000000000000001
 a=0
 b=0
 c=0
 d=0
-
+e=0
+f=0
+g=0
+h=0
 sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind((IPSERV,PORT))
 while True:
-        data,addr=sock.recvfrom(10)
+        data,addr=sock.recvfrom(15)
         print "id : ",ord(data[0])
         print "leng : ",ord(data[1])
         print "gv : ",ord(data[2])
@@ -21,6 +27,13 @@ while True:
         b=(lat>>16)&0xFF
         c=(lat>>8)&0XFF
         d=lat&0xFF
-        
-        sock.sendto(bytearray([88,4,10,180,001,a,b,c,d]),addr)
+        longi=(int)(longi*1000000)
+        longi=longi+longi1
+        longi=longi+longi2
+        e=(longi>>24)&0XFF
+        f=(longi>>16)&0xFF
+        g=(longi>>8)&0XFF
+        h=longi&0xFF
+        sock.sendto(bytearray([88,4,10,180,001,a,b,c,d,e,f,g,h]),addr)
 
+#48.409484,-4.487820
